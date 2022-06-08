7b41b37c757ac8cb9efc7cbc5e6b02c8cc5758fb
meminoztrk <meminoztrk@gmail.com>
2022-06-08 20:54:01 +0300

    product filter until dynamic feature

--- src/containers/ProductFilter.js
+++ src/containers/ProductFilter.js
@@ -34,12 +34,23 @@ const ProductFilter = (props) => {
         });
     }, [location.pathname])
 
-    const onSearch = (value) => console.log(value);
+    const onSearch = (value) => props.filter('search', value, true, false);
+
+    const onRadioClick = (val,min,max) => {
+        if(value){
+            setValue(0);
+            props.filter('price',null,false,true);
+        }else{
+            props.filter('price',{min:min,max:max},false,true);
+            setValue(val);
+        }
+        
+    }
 
 
     return (
         <div className='w-1/5 bg-white rounded-md py-2 border-gray-300 border-opacity-100 border-x-2 border-y shadow divide-y-2'>
-            <Collapse bordered={false} className="bg-white font-medium gap-y-5 pb-12 site-collapse-custom-collapse" expandIconPosition="right" defaultActiveKey={['1', '2', '3', '4', '5', '6', '7', "a0","a1","a2","a3"]}>
+            <Collapse bordered={false} className="bg-white font-medium gap-y-5 pb-12 site-collapse-custom-collapse" expandIconPosition="right" defaultActiveKey={['1', '2', '3', '4', '5', '6', '7', "a0", "a1", "a2", "a3"]}>
                 <Panel header="Kategoriler" key="1">
                     <div className='text-xs font-light pb-4'>
                         <CustomTreeNode treeData={props.features.treeData} />
@@ -54,6 +65,7 @@ const ProductFilter = (props) => {
                                     checked={checkBoxes.brands[index] ? true : false}
                                     onChange={e => onChangeBox(e, index, 'brands')}
                                     key={index}
+                                    onClick={() => props.filter('brand', item, false, false)}
                                     className='text-xs'>
                                     {item}
                                 </Checkbox>
@@ -75,18 +87,18 @@ const ProductFilter = (props) => {
                             <Input type="number" value={price.min} onChange={(e) => setPrice({ ...price, min: e.target.value })} style={{ width: 95, textAlign: 'center', borderRadius: 0 }} size="medium" placeholder="En az" />
                             <Input className="site-input-split" style={{ width: 30, borderLeft: 0, borderRight: 0, pointerEvents: 'none', borderRadius: 0 }} placeholder="-" size="medium" disabled />
                             <Input type="number" value={price.max} onChange={(e) => setPrice({ ...price, max: e.target.value })} className="site-input-right" size="medium" style={{ width: 95, textAlign: 'center', borderRadius: 0 }} placeholder="En çok" />
-                            <Button size='medium' className='bg-orange-500 border-0 rounded-none rounded-r-md' type="primary">{">"}</Button>
+                            <Button size='medium' onClick={()=>{props.filter('price',{min:price.min,max: price.max},false,true);setValue(0)}} className='bg-orange-500 border-0 rounded-none rounded-r-md' type="primary">{">"}</Button>
                         </div>
                         <Radio.Group className='font-light pt-2 pl-1 pb-4' value={value}>
                             <Space direction="vertical">
-                                <Radio value={1} onClick={() => setValue(1)} className="text-xs">750 TL Altında</Radio>
-                                <Radio value={2} onClick={() => setValue(2)} className="text-xs">750 - 1000 TL</Radio>
-                                <Radio value={4} onClick={() => setValue(4)} className="text-xs">1000 - 1500 TL</Radio>
-                                <Radio value={5} onClick={() => setValue(5)} className="text-xs">1500 - 2000 TL</Radio>
-                                <Radio value={6} onClick={() => setValue(6)} className="text-xs">2000 - 3000 TL</Radio>
-                                <Radio value={7} onClick={() => setValue(7)} className="text-xs">3000 - 4000 TL</Radio>
-                                <Radio value={8} onClick={() => setValue(8)} className="text-xs">4000 - 5000 TL</Radio>
-                                <Radio value={9} onClick={() => setValue(9)} className="text-xs">5000 TL üzerinde</Radio>
+                                <Radio value={1} onClick={() => onRadioClick(1, 0, 750)} className="text-xs">750 TL Altında</Radio>
+                                <Radio value={2} onClick={() => onRadioClick(2,750,1000)} className="text-xs">750 - 1000 TL</Radio>
+                                <Radio value={3} onClick={() => onRadioClick(3,1000,1500)} className="text-xs">1000 - 1500 TL</Radio>
+                                <Radio value={4} onClick={() => onRadioClick(4,1500,2000)} className="text-xs">1500 - 2000 TL</Radio>
+                                <Radio value={5} onClick={() => onRadioClick(5,2000,3000)} className="text-xs">2000 - 3000 TL</Radio>
+                                <Radio value={6} onClick={() => onRadioClick(6,3000,4000)} className="text-xs">3000 - 4000 TL</Radio>
+                                <Radio value={7} onClick={() => onRadioClick(7,4000,5000)} className="text-xs">4000 - 5000 TL</Radio>
+                                <Radio value={8} onClick={() => onRadioClick(8,5000,999999)} className="text-xs">5000 TL üzerinde</Radio>
                             </Space>
                         </Radio.Group>
                     </div>
@@ -110,7 +122,7 @@ const ProductFilter = (props) => {
 
                 {typeof props.features.values != "function" && props.features.values != null ?
                     props.features.values.map((x, i) => (
-                        <Panel header={x.name} key={"a"+i}>
+                        <Panel header={x.name} key={"a" + i}>
                             <Space direction="vertical" className='font-light pl-1 pb-4'>
                                 {x.values.map((item, index) => (
                                     <Checkbox
@@ -135,6 +147,7 @@ const ProductFilter = (props) => {
                                     checked={checkBoxes.colors[index] ? true : false}
                                     onChange={e => onChangeBox(e, index, 'colors')}
                                     key={index}
+                                    onClick={() => props.filter('color', item, false, false)}
                                     className='text-xs'>
                                     {item}
                                 </Checkbox>
@@ -151,6 +164,7 @@ const ProductFilter = (props) => {
                             <Checkbox
                                 checked={checkBoxes.status[index] ? true : false}
                                 onChange={e => onChangeBox(e, index, 'status')}
+                                onClick={() => props.filter('status', item === "Sıfır" ? false : true, false, false)}
                                 key={index}
                                 value={index}
                                 className='text-xs'>
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -4,56 +4,120 @@ import ProductList from '../containers/ProductList'
 import { useLocation, Link } from 'react-router-dom';
 import { Breadcrumb } from 'antd';
 
-
+let allProducts = [];
 const Products = () => {
-    const location = useLocation();
-    const [breadcrumb, setBreadcrumb] = useState([]);
-    const [products, setProducts] = useState([]);
-    const [productNav, setProductNav] = useState([]);
-    const [features, setFeatures] = useState([]);
+  const location = useLocation();
+  const [breadcrumb, setBreadcrumb] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [productNav, setProductNav] = useState([]);
+  const [features, setFeatures] = useState([]);
+  const [filter, setFilter] = useState({});
 
-    const getAllData = async (loc) => {
-        await fetch(process.env.REACT_APP_API + "/Products/GetProductsByCategoryName", {
-          method: 'POST',
-          body: JSON.stringify(loc),
-          headers: {
-            'ApiKey': process.env.REACT_APP_API_KEY,
-            'Content-Type': 'application/json'
-          }
-        })
-          .then(res => res.json())
-          .then(data => {
-            console.log(data.data);
-            setBreadcrumb(data.data.navigation);
-            setProducts(data.data.products);
-            setProductNav(data.data.productNav)
-            setFeatures(data.data.productFeatures);
-          })
+  const getAllData = async (loc) => {
+    await fetch(process.env.REACT_APP_API + "/Products/GetProductsByCategoryName", {
+      method: 'POST',
+      body: JSON.stringify(loc),
+      headers: {
+        'ApiKey': process.env.REACT_APP_API_KEY,
+        'Content-Type': 'application/json'
       }
+    })
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data.data);
+        setBreadcrumb(data.data.navigation);
+        setProducts(data.data.products);
+        allProducts = data.data.products;
+        setProductNav(data.data.productNav)
+        setFeatures(data.data.productFeatures);
+      })
+  }
 
-    useEffect(() => {
-        let categories = location.pathname.split('/')
-        categories.splice(0, 1); categories.splice(0, 1);
-        console.log(categories);
-        getAllData(categories);
-    }, [location])
+  const pFilters = (filt, value, isSearch, isPrice) => {
+    let newItem = { ...filter };
 
-    return (
-        <div className='md:container mx-auto py-4'>
-            <Breadcrumb className='text-xs' separator=">">
-                <Breadcrumb.Item href="#">Ana Sayfa</Breadcrumb.Item>
-                {breadcrumb.map((item, index) => {
-                    return (breadcrumb.length - 1 !== index ? 
-                        <Breadcrumb.Item key={index}><Link to={item.path}>{item.name}</Link></Breadcrumb.Item> : <Breadcrumb.Item key={index}>{item.name}</Breadcrumb.Item>)
-                })}
-            </Breadcrumb>
-            <div className='flex py-4 justify-between space-x-4 h-full'>
-                <ProductFilter features={features} />
-                <ProductList products={products} nav={productNav} />
-            </div>
+    if(isSearch){
+      if(value.length > 0){
+        setFilter({...filter, search: value})
+      }
+      else{
+        delete newItem[filt]
+        setFilter(newItem)
+      }
+    }
+    else if(isPrice){
+      if(value != null){
+        setFilter({...filter, price: value})
+      }else{
+        delete newItem[filt]
+        setFilter(newItem)
+      }
+      
+    }
+    else{
+      if (filter[filt] === undefined) {
+        setFilter({ ...filter, [filt]: [value] })
+      }
+      else {
+        newItem[filt].includes(value) ? newItem[filt].splice(newItem[filt].indexOf(value), 1) : newItem[filt].push(value);
+        newItem[filt].length < 1 && delete newItem[filt];
+        setFilter(newItem)
+      }
+    }
+    
+  };
 
-        </div>
-    )
+  useEffect(() => {
+    let lastdata = [];
+
+    console.log(filter)
+    lastdata = lastdata.concat(allProducts.filter(x =>
+      (!filter.brand || filter.brand.length < 1 || filter.brand.includes(x.brand)) &&
+      (!filter.color || filter.color.length < 1 || filter.color.includes(x.color)) &&
+      (!filter.status || filter.status.length < 1 || filter.status.includes(x.status)) &&
+      (!filter.price || (x.price > filter.price.min && x.price < filter.price.max)) &&
+      (!filter.search || filter.search.length < 1 || x.name.toLowerCase().includes(filter.search.toLowerCase())) 
+    ));
+
+    setProducts(Object.keys(filter).length < 1 ? allProducts : lastdata);
+    setProductNav({categoryName: productNav.categoryName,   productCount: lastdata.length});  
+  }, [filter]);
+
+  // useEffect(() => {
+  //   let lastdata = [];
+  //   for (var key in filter) {
+  //     filter[key].map(item => {
+  //       lastdata = lastdata.concat(allProducts.filter(x => x[key] === item))
+  //     })
+  //   }
+  //   console.log(lastdata, allProducts);
+  //   setProducts(lastdata);
+  // }, [filter]);
+
+
+  useEffect(() => {
+    let categories = location.pathname.split('/')
+    categories.splice(0, 1); categories.splice(0, 1);
+    getAllData(categories);
+    setFilter({})
+  }, [location])
+
+  return (
+    <div className='md:container mx-auto py-4'>
+      <Breadcrumb className='text-xs' separator=">">
+        <Breadcrumb.Item href="#">Ana Sayfa</Breadcrumb.Item>
+        {breadcrumb.map((item, index) => {
+          return (breadcrumb.length - 1 !== index ?
+            <Breadcrumb.Item key={index}><Link to={item.path}>{item.name}</Link></Breadcrumb.Item> : <Breadcrumb.Item key={index}>{item.name}</Breadcrumb.Item>)
+        })}
+      </Breadcrumb>
+      <div className='flex py-4 justify-between space-x-4 h-full'>
+        <ProductFilter features={features} filter={pFilters} />
+        <ProductList products={products} nav={productNav} />
+      </div>
+
+    </div>
+  )
 }
 
 export default Products
